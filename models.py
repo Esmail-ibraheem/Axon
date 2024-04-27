4ca6a454c4924338dd746b095e6538c98c7a02c0
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-04-27 09:28:16 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 @dataclass
 class TransformerConfig:
     source_vocab_size: int
-    target_vocab_size: int 
+    target_vocab_size: int  
     source_sequence_length: int 
     target_sequence_length: int 
     d_model: int = 512
