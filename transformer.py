780ab463dab5bb03500b886e33fb8e9010c72b46
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-04-29 22:01:10 +0300

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,4 +1,4 @@
-# an implementation of the llama 2 from scratch and train it using reinforcement learning from human feedback 
+# an implementation of the llama 2 from scratch
 from dataclasses import dataclass
 import math
 import torch
