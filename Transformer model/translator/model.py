e2dc0db396bbb11a60571b041c48755f3d60df7b
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-05-21 09:14:03 +0300

    Update model.py

--- Transformer model/translator/model.py
+++ Transformer model/translator/model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
    
 class InputEmbeddingsLayer(nn.Module):   
     def __init__(self, d_model: int, vocab_size: int) -> None:  
-        super().__init__()
+        super().__init__() 
         self.d_model = d_model 
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model) 
