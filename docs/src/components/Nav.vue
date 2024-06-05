2e6076edc0f68a65fbc76a7e2c9075288a48e399
Hetari <hetari4py@gmail.com>
2024-06-05 21:55:38 +0300

    finish the navbar

--- docs/src/components/Nav.vue
+++ docs/src/components/Nav.vue
@@ -1,17 +1,36 @@
 <template>
   <header>
     <nav class="common-padding flex flex-row justify-between items-center">
-      <p class="text-xl font-logo">
-        <a href="#">Axon</a>
+      <p class="text-xl font-logo group flex-center w-14">
+        <a
+          class="tracking-normal group-hover:tracking-[.20em] transition-all duration-300"
+          href="#"
+          >Axon</a
+        >
       </p>
 
       <ul class="flex gap-5">
-        <li class="nav-link">
-          <a
-            href="https://github.com/Esmail-ibraheem/Axon?tab=readme-ov-file#axons-packages">
-            Packages
-          </a>
-        </li>
+        <span
+          class="flex gap-5"
+          v-if="width > 768">
+          <li
+            v-for="p in packages"
+            :key="p.name"
+            class="nav-link">
+            <a :href="p.url">
+              {{ p.name }}
+            </a>
+          </li>
+        </span>
+
+        <span v-else>
+          <li class="nav-link">
+            <a
+              href="https://github.com/Esmail-ibraheem/Axon?tab=readme-ov-file#axons-packages">
+              Packages
+            </a>
+          </li>
+        </span>
 
         <!-- Github -->
         <li class="nav-link">
@@ -40,7 +59,7 @@
 
         <!-- Linkedin -->
         <li class="nav-link">
-          <a href="http://linkedin.com/in/esmail-a-gumaan">
+          <a href="https://www.linkedin.com/in/esmail-a-gumaan/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -76,23 +95,25 @@
 
         <!-- email -->
         <li class="nav-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="20"
-            height="20"
-            fill="none">
-            <path
-              d="M2 6L8.91302 9.91697C11.4616 11.361 12.5384 11.361 15.087 9.91697L22 6"
-              stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linejoin="round" />
-            <path
-              d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"
-              stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linejoin="round" />
-          </svg>
+          <a href="mailto:esm.agumaan@gmail.com">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="20"
+              height="20"
+              fill="none">
+              <path
+                d="M2 6L8.91302 9.91697C11.4616 11.361 12.5384 11.361 15.087 9.91697L22 6"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linejoin="round" />
+              <path
+                d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linejoin="round" />
+            </svg>
+          </a>
         </li>
 
         <li
@@ -139,7 +160,28 @@
 
 <script setup>
   import { useDark, useToggle } from '@vueuse/core';
+  import { useWindowSize } from '@vueuse/core';
 
+  const { width, height } = useWindowSize();
   const isDark = useDark();
   const toggleDark = useToggle(isDark);
+
+  const packages = [
+    {
+      name: 'Transformer',
+      url: 'https://github.com/Esmail-ibraheem/Axon/tree/main/Transformer%20model',
+    },
+    {
+      name: 'X-Llama',
+      url: 'https://github.com/Esmail-ibraheem/Axon/tree/main/X-Llama',
+    },
+    {
+      name: 'Dali',
+      url: 'https://github.com/Esmail-ibraheem/Axon/tree/main/Dali',
+    },
+    {
+      name: 'InstructGPT ',
+      url: 'https://github.com/Esmail-ibraheem/Axon/tree/main/RLHF',
+    },
+  ];
 </script>
