c395ffa4a1eaf5b64f0fa7167910b2933652588b
Hetari <hetari4py@gmail.com>
2024-07-16 20:59:46 +0300

    try to fix vue action

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Axon",
+  publicPath: 'Axon',
   plugins: [vue()],
   resolve: {
     alias: {
